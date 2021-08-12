c266ba019acda4b0eda15ee543881ef4dba20f84
Kartikey0212 <84659938+Kartikey0212@users.noreply.github.com>
2021-08-12 10:07:02 +0530

    Add files via upload

---
+++ pptExport/Controllers/PptController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pptExport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PptController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+        public PptController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOwners()
+        {
+            try
+            {
+                var owners = _repository.Owner.GetAllOwners();
+                _logger.LogInfo($"Returned all owners from database.");
+                return Ok(owners);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllOwners action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
